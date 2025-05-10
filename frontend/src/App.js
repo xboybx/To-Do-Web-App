1a20900175341cefb2f1d6c1f6f8c4b386683a64
Jaswanth <jeswanth012@gmail.com>
2025-05-10 22:11:11 +0530

    mobile responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -125,15 +125,15 @@ function App() {
         </button>
       </nav>
 
-      <div className='w-full px-16 '>
-        <TaskList
-          tasks={filteredTasks()}
-          onToggleComplete={toggleComplete}
-          onEdit={startEdit}
-          onDelete={handleDelete}
-          onToggleStar={toggleStar}
-        />
-      </div>
+
+      <TaskList
+        tasks={filteredTasks()}
+        onToggleComplete={toggleComplete}
+        onEdit={startEdit}
+        onDelete={handleDelete}
+        onToggleStar={toggleStar}
+      />
+
 
       {isModalOpen && (
         <Modal onClose={() => setIsModalOpen(false)}>
