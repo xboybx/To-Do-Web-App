654feb6b5546efff19dc9ce0c818dffca8e0da1b
Jaswanth <jeswanth012@gmail.com>
2025-05-11 13:06:31 +0530

    added loading spinner

--- frontend/src/components/LoadingScreen.js
+++ frontend/src/components/LoadingScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { SyncLoader } from 'react-spinners';
+import { FadeLoader } from 'react-spinners';
 
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 flex flex-col justify-center items-center bg-gray-900 bg-opacity-90 z-50">
-      <SyncLoader color="white" size={20} />
+      <FadeLoader color="#8899A6" size={-50} />
       {/* <h2 className="text-center text-white text-xl font-semibold mt-4">Loading tasks...</h2> */}
     </div>
   );
