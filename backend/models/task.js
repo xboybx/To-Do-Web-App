a40c94d63ca4477e4e4ce0104c263c1e95f93d48
Jaswanth <jeswanth012@gmail.com>
2025-05-11 12:30:07 +0530

    added star field in modal

--- backend/models/task.js
+++ backend/models/task.js
@@ -5,6 +5,7 @@ const taskSchema = new mongoose.Schema({
     date: { type: Date, default: null },
     priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
     completed: { type: Boolean, default: false },
+    starred: { type: Boolean, default: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Task', taskSchema);
