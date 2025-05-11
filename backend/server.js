9647e415e77d8129be55b81b16471b5abaa38baf
Jaswanth <jeswanth012@gmail.com>
2025-05-11 11:41:15 +0530

    Remved condole logger

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Request logging middleware
-app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-});
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
